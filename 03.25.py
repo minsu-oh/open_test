e3d8d3635ce4e23695a3011614283610dee82693
minsu <dhals0517@naver.com>
2025-03-27 15:52:00 +0900

    forth try

--- 03.25.py
+++ 03.25.py
@@ -12,11 +12,9 @@ class Calculator(QWidget):
         self.resize(256,256)
         self.show()
 
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     view = Calculator()
     sys.exit(app.exec_())
-
-
-
-    
